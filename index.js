652707e8f34fb07cd964e61791e366f98d601b12
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-01-30 16:34:17 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let message = "";
+  city === "NYC" ? (message = "Ok, sounds good.") : (message = "No go.");
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(m) {
   // Write your code here!
+  switch (m) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    case "thanks for everything":
+      return "Bye.";
+  }
 }
